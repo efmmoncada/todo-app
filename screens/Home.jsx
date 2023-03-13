2261b57d2d9ed640e9fb5fbf92b0b7e754991e1c
efmmoncada <emmanuel.moncada27@gmail.com>
2023-03-13 00:01:53 -0700

    save app state to storage and load on launch
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -6,12 +6,26 @@ import ItemDisplay from '../item/ItemDisplay';
 export function Home({ tasks, setTasks }) {
     return (
         <View style={styles.container}>
-            <ScrollView keyboardShouldPersistTaps='handled' contentContainerStyle={styles.container} automaticallyAdjustKeyboardInsets={true}>
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={styles.container}
+                automaticallyAdjustKeyboardInsets={true}
+            >
                 <DateDisplay />
-                {tasks.length ?
+                {tasks.length ? (
                     tasks.map((task) => (
-                        <ItemDisplay nCompleted={task.completed} nGoal={task.goal} taskName={task.taskName} setTasks={setTasks} tasks={tasks}/>
-                    )) : <Text>No Tasks Available</Text>}
+                        <ItemDisplay
+                            key={task.taskName}
+                            nCompleted={task.completed}
+                            nGoal={task.goal}
+                            taskName={task.taskName}
+                            setTasks={setTasks}
+                            tasks={tasks}
+                        />
+                    ))
+                ) : (
+                    <Text>No Tasks Available</Text>
+                )}
             </ScrollView>
             <StatusBar style="auto" />
         </View>
